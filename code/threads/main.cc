bd1fb1e34168b861f12901eaf47a5d3c06a48924
ouarglir <racim.ouargli@etu.univ-grenoble-alpes.fr>
2025-12-17 11:34:49 +0100

    Modification: main.cc

--- code/threads/main.cc
+++ code/threads/main.cc
@@ -107,7 +107,7 @@ int main(int argc, char **argv) {
             interrupt->Halt(); // once we start the console, then
                                // Nachos will loop forever waiting
                                // for console input
-        } /*else if (!strcmp(*argv, "-sc")){
+        } else if (!strcmp(*argv, "-sc")){
             if (argc == 1)
                 SynchConsoleTest(NULL, NULL);
             else {
@@ -115,7 +115,7 @@ int main(int argc, char **argv) {
                 SynchConsoleTest(*(argv + 1), *(argv + 2));
                 argCount = 3;
             }
-        }*/
+        }
 #endif // USER_PROGRAM
 #ifdef FILESYS
         if (!strcmp(*argv, "-cp")) { // copy from UNIX to Nachos
