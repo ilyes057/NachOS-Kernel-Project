58d863ac9647bb1ee3941adfbc9491643a77f9c2
ouarglir <racim.ouargli@etu.univ-grenoble-alpes.fr>
2025-12-19 09:44:42 +0100

    Correction

--- code/test/putchar.c
+++ code/test/putchar.c
@@ -1,15 +1,8 @@
 #include "syscall.h"
 
-void print(char c, int n)
-{
-    int i;
-    for (i = 0; i < n; i++) {
-        PutChar(c+i);
-    }
-    PutChar('\n');
-}
 int main()
 {
-    print('a',4);
+    PutChar('a');
+    PutChar('\n');
     return -1;
 }
